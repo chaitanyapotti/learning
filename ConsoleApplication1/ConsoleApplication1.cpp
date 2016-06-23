1a17eea223b82e99512323630b0922b9ba3c6161
chaitanyapotti <chaitanya.potti@gmail.com>
2016-06-23 20:43:58 +0530

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world.! \n";
+	//return 0;
+	// Sample code for cpp
+	/*int a = 12;
+	double b = 24.0;
+	double c = a + b;
+	double y = 1;
+	double i = 2.0;
+	while (true)
+	{
+		double fx = 100.0 * i * i - pow(2, i);
+		if (fx < 0)
+		{
+			y = i;
+			break;
+		}
+		i++;
+
+	}
+	cout << y;*/
+
+	//Code for Insertion Sort order n^2
+	int arr[8] = { 10,3,5,9,6,2,7,6 };
+
+	int foo[8];
+	int key;
+	for (int i = 1; i < 8; i++)
+	{
+		key = arr[i];
+
+	}
+
+
+}
+
