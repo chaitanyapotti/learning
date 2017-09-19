0b4822e364fd248286a1efd1f3c0a1d706006132
chaitanya potti <chaitanya.potti@gmail.com>
2017-09-19 09:56:40 +0530

    Post chapter 3

---
+++ Lesson1/ItemTemplateSelectorCombo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lesson1
+{
+    class ItemTemplateSelectorCombo : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var control = container as FrameworkElement;
+            var person = item as Person;
+            return person.Age > 18 ? control.FindResource("Normal") as DataTemplate : control.FindResource("AbNormal") as DataTemplate;
+
+        }
+    }
+}
--- Lesson1/MainWindow.xaml.cs
+++ Lesson1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,35 @@ namespace Lesson1
         public MainWindow()
         {
             InitializeComponent();
+            //ComboBox.DataContext = this;
+            list.Add(new Person { Age = 16, Name = "John" });
+            list.Add(new Person { Age = 19, Name = "Julie" });
+            //ComboBox.ItemsSource = list;
+            Panel.AddHandler(Button.ClickEvent, new RoutedEventHandler(btn_Click));
+
+            CommandBinding binding = new CommandBinding(ApplicationCommands.Copy);
+            CommandBindings.Add(binding);
+            binding.Executed += (o, args) => Debug.WriteLine("Command Executed");
         }
+
+        public void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = e.Source as Button;
+            btn.Background = Brushes.AliceBlue;
+        }
+
+        public List<Person> list { get; set; } = new List<Person>();
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("Mouse Down" + sender);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
