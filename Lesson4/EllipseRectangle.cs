588f5378b75733fb90a02cefd0b2e49873ac6eac
chaitanya potti <chaitanya.potti@gmail.com>
2017-09-20 09:17:46 +0530

    h

---
+++ Lesson4/EllipseRectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Lesson4
+{
+    class EllipseRectangle : FrameworkElement
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            Rect r = new Rect(0, 0, ActualWidth, ActualHeight);
+            drawingContext.DrawRectangle(Brushes.Red, null, r);
+
+            Point centerPoint = new Point(ActualWidth / 2, ActualHeight / 2);
+            drawingContext.DrawEllipse(Brushes.Blue, new Pen(Brushes.Black, 4), centerPoint, ActualWidth / 2 - 4, ActualHeight / 2 - 4);
+        }
+    }
+}
