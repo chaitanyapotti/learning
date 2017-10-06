0b6e94a94c1c4b12df583cd05bac1326b6b70d09
chaitanya potti <chaitanya.potti@gmail.com>
2017-10-06 10:32:34 +0530

    Graphics complete + Data Binding

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Binding binding = new Binding("CurrentCulture") {Source = Thread.CurrentThread};
+            BindingOperations.SetBinding(Block, TextBlock.TextProperty, binding);
+            // or Block.SetBinding(TextBlock.TextProperty, binding);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
