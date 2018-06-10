1d1b5ca34a8e54afb17d4e1f8261076a4e89ff27
chaitanya potti <chaitanya.potti@gmail.com>
2018-06-10 10:39:58 +0530

    QuickStart to Javascript complete

---
+++ QuickStart to Javascript/script2.js
@@ -0,0 +1,23 @@
+﻿//Coffee Objects
+let myCoffee = {
+    flavor: "espresso",
+    temperature: "piping hot",
+    ounces: 100,
+    milk: true,
+    reheat: function () {
+        if (this.temperature === "cold") {
+            this.temperature = "piping hot";
+            alert("Your coffee has been reheated");
+        }
+    }
+}
+
+myCoffee.temperature = "cold";
+myCoffee.reheat();
+
+
+class Friend {
+    constructor Friend(name, tShirtColor) {
+
+    }
+}
